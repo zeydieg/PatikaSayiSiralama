f69a014139a9e37de2d4753aea0bd9358d55bb52
Eylül Güler <zeynep_eylul_guler@hotmail.com>
2022-05-26 00:35:31 +0300

    first commit

---
+++ src/sayiSiralama.java
@@ -0,0 +1,36 @@
+import javax.swing.plaf.synth.SynthLookAndFeel;
+import java.util.Scanner;
+
+public class sayiSiralama {
+    public static void main(String[] args) {
+    Scanner inp = new Scanner(System.in);
+    int a, b, c;
+
+    System.out.print("1. sayıyı giriniz : ");
+    a = inp.nextInt();
+    System.out.print("2. sayıyı giriniz : ");
+    b = inp.nextInt();
+    System.out.print("3. sayıyı giriniz : ");
+    c = inp.nextInt();
+
+    if ((a < b) && (a < c)) {
+        if (b < c) {
+        System.out.print("a < b < c");
+    } else {
+        System.out.print("a < c < b");
+    }
+    } else if ((b < a) && (b < c)) {
+        if (a < c) {
+            System.out.print("b < a < c");
+        } else {
+            System.out.print("b < c < a");
+        }
+    } else {
+        if (a < b) {
+            System.out.print("c < a < b");
+        } else {
+            System.out.print("c < b < a");
+        }
+    }
+}
+}
